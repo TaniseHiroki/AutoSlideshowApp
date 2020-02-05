f3c50d12438bc05f933cbee402db0ac849820207
TaniseHiroki <tanise@scsk.jp>
2020-02-05 16:02:35 +0900

    レビュー指摘対応（2回目） ・パーミッションを拒否した際に、「再生/停止」ボタンを押せなくしました。

--- app/src/main/java/jp/techacademy/hiroki/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/hiroki/autoslideshowapp/MainActivity.kt
@@ -68,47 +68,51 @@ class MainActivity : AppCompatActivity() {
         }
 
         nextbutton.setOnClickListener{
-            val resolver = contentResolver
-            val cursor = resolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                null,
-                null,
-                null,
-                null
-            )
-            if(imageArrayList.size - 1 == imageArrayListposition){
-                imageArrayListposition = 0
-            } else {
-                imageArrayListposition++
-            }
-            val id = imageArrayList[imageArrayListposition]
-            val imageUri =
-                ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+            if(imageArrayList.size != 0) {
+                val resolver = contentResolver
+                val cursor = resolver.query(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    null,
+                    null,
+                    null,
+                    null
+                )
+                if (imageArrayList.size - 1 == imageArrayListposition) {
+                    imageArrayListposition = 0
+                } else {
+                    imageArrayListposition++
+                }
+                val id = imageArrayList[imageArrayListposition]
+                val imageUri =
+                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
-            imageView.setImageURI(imageUri)
-            cursor.close()
+                imageView.setImageURI(imageUri)
+                cursor.close()
+            }
         }
 
-        backbutton.setOnClickListener{
-            val resolver = contentResolver
-            val cursor = resolver.query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                null,
-                null,
-                null,
-                null
-            )
-            if(imageArrayListposition == 0){
-                imageArrayListposition = imageArrayList.size - 1
-            } else {
-                imageArrayListposition--
-            }
-            val id = imageArrayList[imageArrayListposition]
-            val imageUri =
-                ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+        backbutton.setOnClickListener {
+            if (imageArrayList.size != 0) {
+                val resolver = contentResolver
+                val cursor = resolver.query(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    null,
+                    null,
+                    null,
+                    null
+                )
+                if (imageArrayListposition == 0) {
+                    imageArrayListposition = imageArrayList.size - 1
+                } else {
+                    imageArrayListposition--
+                }
+                val id = imageArrayList[imageArrayListposition]
+                val imageUri =
+                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
-            imageView.setImageURI(imageUri)
-            cursor.close()
+                imageView.setImageURI(imageUri)
+                cursor.close()
+            }
         }
     }
 
@@ -174,6 +178,7 @@ class MainActivity : AppCompatActivity() {
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Log.d("ANDROID10", "許可された")
                 } else {
+                    slidebutton.isEnabled = false
                     Log.d("ANDROID10", "許可されなかった")
                 }
         }
